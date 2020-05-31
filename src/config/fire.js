3ba1b49c8a8d79d1d6c5f57c48b3638ac26bd2a6
treblegni <ingelbert.figueroa@gmail.com>
2020-05-30 18:36:12 -0700

    removed sensitive information

--- src/config/fire.js
+++ src/config/fire.js
@@ -3,13 +3,6 @@
 import firebase from 'firebase'
 
 const config = {
-        apiKey: "AIzaSyAKM9hlwY3DSgC9KtQs2fKAOvmIeHQJNRA",
-        authDomain: "accord-7789b.firebaseapp.com",
-        databaseURL: "https://accord-7789b.firebaseio.com",
-        projectId: "accord-7789b",
-        storageBucket: "accord-7789b.appspot.com",
-        messagingSenderId: "255745495487",
-        appId: "1:255745495487:web:6fa59c9410503bcb"
     }
 
 // Initialize Firebase
